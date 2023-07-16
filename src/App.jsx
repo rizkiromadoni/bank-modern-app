647f690a681b438049fd998b29ff28fab4377eb6
rizkiromadoni <coradex06@gmail.com>
2023-07-16 18:42:48 +0700

    another configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import style from './style'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+
+      <div className={`${style.paddingX} ${style.flexCenter}`} >
+        <div className={`${style.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div className={`bg-primary ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          Stats
+          Bussiness
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
+
+    </div>
   )
 }
 
---
+++ src/components/Bussiness.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Bussiness = () => {
+  return (
+    <div>Bussiness</div>
+  )
+}
+
+export default Bussiness
