c8a0167c577c47409c457faa27f8d1c1d5a247b8
rizkiromadoni <coradex06@gmail.com>
2023-07-18 12:58:48 +0700

    CTA component and fix bug

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import style from "../style"
+import Button from "./Button"
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <section className={`${style.flexCenter} ${style.marginY} ${style.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className="flex-1 flex flex-col">
+        <h2 className={style.heading2}>Let's try our service now!</h2>
+        <p className={`${style.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+      <div className={`${style.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+        <Button />
+      </div>
+    </section>
   )
 }
 
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -4,7 +4,7 @@ import style from "../style"
 const Clients = () => {
   return (
     <section className={`${style.flexCenter} my-4`}>
-      <div className={`${style.flexCenter} flex-wrap w-full`}>
+      <div className={`${style.flexCenter} flex-wrap w-full gap-3`}>
         {
           clients.map((client) => (
             <div key={client.id} className={`flex-1 ${style.flexCenter} sm:min-w-[192px]`}>
